44ea6b78b9780938cffddb1be9bc7241e8a312ab
simolex <simolex@yandex.ru>
2022-02-09 10:04:18 +0300

    Восстановлен порядок глав

--- script.js
+++ script.js
@@ -14,6 +14,18 @@ body.style.backgroundImage = 'url("./image/you-dont-know-js.jpg")';
 const titleBook_3 = bookList[4].querySelector("h2>a");
 titleBook_3.textContent = "Книга 3. this и Прототипы Объектов";
 adware.remove();
-console.log(titleBook_3);
+//console.log(titleBook_3);
+
+const bookContentTwo = bookList[0].querySelectorAll("ul>li");
+bookContentTwo[10].before(bookContentTwo[2]);
+bookContentTwo[3].after(bookContentTwo[6]);
+bookContentTwo[4].before(bookContentTwo[8]);
+
+const bookContentFive = bookList[5].querySelectorAll("ul>li");
+bookContentFive[1].after(bookContentFive[9]);
+bookContentFive[6].before(bookContentFive[2]);
+bookContentFive[8].before(bookContentFive[5]);
+
+console.log(bookContentTwo, bookContentFive);
 
 //bookList
