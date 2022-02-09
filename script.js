f6350ee230785445467a85f9463619831588aa24
simolex <simolex@yandex.ru>
2022-02-09 09:49:12 +0300

    Исправлен заголовок и удалена реклама

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const body = document.querySelector("body");
+const adware = document.querySelector(".adv");
 const booksSection = document.querySelector(".books");
 const bookList = document.querySelectorAll(".book");
 
-console.log(booksSection);
 console.log(bookList);
 
 booksSection.insertAdjacentElement("afterbegin", bookList[1]);
@@ -10,5 +10,10 @@ booksSection.insertAdjacentElement("beforeend", bookList[2]);
 bookList[3].before(bookList[4]);
 
 body.style.backgroundImage = 'url("./image/you-dont-know-js.jpg")';
+
+const titleBook_3 = bookList[4].querySelector("h2>a");
+titleBook_3.textContent = "Книга 3. this и Прототипы Объектов";
+adware.remove();
+console.log(titleBook_3);
 
 //bookList
