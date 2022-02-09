b4b7d5ddc42f8c615b3c0e952fe0e2fd9e4c4725
simolex <simolex@yandex.ru>
2022-02-09 09:27:18 +0300

    Изменен задний фон

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body");
 const booksSection = document.querySelector(".books");
 const bookList = document.querySelectorAll(".book");
 
@@ -7,5 +8,7 @@ console.log(bookList);
 booksSection.insertAdjacentElement("afterbegin", bookList[1]);
 booksSection.insertAdjacentElement("beforeend", bookList[2]);
 bookList[3].before(bookList[4]);
+
+body.style.backgroundImage = 'url("./image/you-dont-know-js.jpg")';
 
 //bookList
